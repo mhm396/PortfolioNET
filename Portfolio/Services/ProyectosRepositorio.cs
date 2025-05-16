e4e6c68e73bfebf4e8cb12f618615f0081ee200b
mhm396 <mhm396@inlumine.ual.es>
2025-05-16 02:10:42 +0200

    Ultimos retoques

--- Portfolio/Services/ProyectosRepositorio.cs
+++ Portfolio/Services/ProyectosRepositorio.cs
@@ -16,32 +16,48 @@ namespace Portfolio.Services
             {
                 new Proyecto()
                 {
-                    Titulo = "Proyecto 1",
-                    Descripcion = "Descripcion del proyecto 1",
-                    ImagenURL = "/images/car1.jpg",
-                    Link = "https://www.google.com"
+                    Titulo = "Filtros espaciales en imágenes",
+                    Descripcion = "Programa en C++ para preprocesamiento de imágenes mediante filtros como detección de bordes, suavizado y ajuste de contraste.",
+                    ImagenURL = "/images/TDI.png",
+                    Link = "https://github.com/mhm396/TDI-Filtros-espaciales"
+                },
+                    new Proyecto()
+                {
+                    Titulo = "Ransomware post-cuántico",
+                    Descripcion = "Simulación de ataque en Linux con RSA y McEliece, incluyendo un videojuego como fachada para el cifrado.",
+                    ImagenURL = "/images/ransomware.jpg",
+                    Link = "https://github.com/mhm396/Proyecto-RansomwareEducativo"
+                },
+                    new Proyecto()
+                {
+                    Titulo = "SWAPI",
+                    Descripcion = "Backend en Laravel y MySQL para gestionar naves y pilotos de Star Wars, con API REST y comando Artisan para importar datos desde SWAPI.",
+                    ImagenURL = "/images/swapi.png",
+                    Link = "https://github.com/mhm396/SWAPI-BACKEND"
                 },
                 new Proyecto()
                 {
-                    Titulo = "Proyecto 2",
-                    Descripcion = "Descripcion del proyecto 2",
-                    ImagenURL = "/images/donatello1.jpg",
-                    Link = "https://www.google.com"
+                    Titulo = "Infraestructura en GCP",
+                    Descripcion = "Gestión de instancias, contenedores con Kubernetes, copias de seguridad y configuración de firewalls en Google Cloud Platform.",
+                    ImagenURL = "/images/kubernetes.jpg",
+                    Link = "https://github.com/mhm396"
                 },
                 new Proyecto()
                 {
-                    Titulo = "Proyecto 3",
-                    Descripcion = "Descripcion del proyecto 3",
-                    ImagenURL = "/images/habanero-red.jpg",
-                    Link = "https://www.google.com"
+                    Titulo = "Plataforma E-commerce",
+                    Descripcion = "Desarrollo de una plataforma web con PHP y SQL, incluyendo carrito de compras, pasarela de pago y panel de administración.",
+                    ImagenURL = "/images/e-commerce.jpeg",
+                    Link = "https://github.com/mhm396"
                 },
+
                 new Proyecto()
                 {
-                    Titulo = "Proyecto 4",
-                    Descripcion = "Descripcion del proyecto 4",
-                    ImagenURL = "/images/habanero-red.jpg",
-                    Link = "https://www.google.com"
+                    Titulo = "Aplicación Android en PlayStore",
+                    Descripcion = "App en Android Studio y Firebase para gestión de préstamos, recordatorios y notas colaborativas con sincronización en la nube.",
+                    ImagenURL = "/images/REMINDAPP2.jpg",
+                    Link = "https://apkcombo.com/es/remind/com.remindApp.miRemind/"
                 }
+
             };
         }
     }
